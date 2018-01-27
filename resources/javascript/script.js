215b81c9377eda8fa2b9b31ca077453a995cd88a
devsagul <dev.sagul@gmail.com>
2018-01-27 15:41:19 +0300

    binded icons

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -6,13 +6,22 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiZGV2c2FndWwiLCJhIjoiY2pjd2RhMzl0MGdhaDMzcW82eGx1amNpayJ9.9CJ5rqAdw90_Tf9FKj-XBw'
 }).addTo(mymap);
 
+var icons = {
+  temp:L.icon({iconUrl: './resources/icon/Termo.png', iconSize: [18, 50]}),
+  pollution:L.icon({iconUrl: './resources/icon/Dair.png', iconSize: [50, 27]}),
+  electricity:L.icon({iconUrl: './resources/icon/Delec.png', iconSize: [50, 27]}),
+  street_light:L.icon({iconUrl: './resources/icon/Light.png', iconSize: [37, 50]}),
+  parking_lot:L.icon({iconUrl: './resources/icon/P.png', iconSize: [50, 50]}),
+  water: L.icon({iconUrl: './resources/icon/Dwater.png', iconSize: [50, 25]})
+};
+
 mymap.whenReady(function() {
   $.get("/api/sensors/", function( data ) {
     var sensors = $.map(data, function(el) {return el});
     var sensorsLen = sensors.length;
     for (var i = 0; i < sensorsLen; i++) {
       var sensor = sensors[i];
-      var marker = L.marker([sensor["latitude"], sensor["longitude"]]).addTo(mymap);
+      var marker = L.marker([sensor["latitude"], sensor["longitude"]]).addTo(mymap).setIcon(icons[sensor["sensor_type"]]);
       var popupContent = sensor["name"];
       marker.bindPopup(popupContent);
     }
